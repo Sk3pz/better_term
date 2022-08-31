e5f8957b5d389a4f6cd69a0821d6ca7c9471e6ec
eric <skepz.dev@gmail.com>
2022-08-31 13:16:00 -0500

    documentation update

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,14 @@ fn _yn_prompt(p: String) -> bool {
 #[cfg(feature = "input")]
 #[macro_export]
 /// Returns a string from stdin with the prompt given
+/// # Example
+/// ```
+/// use better_term::read_input;
+///
+/// // by default read_input() will prompt with "> "
+/// let input = read_input!();
+/// // specify a custom prompt with the same formatting as format!()
+/// let input2 = read_input!("Enter a second value: ");
 macro_rules! read_input {
     () =>  { $crate::_read_input(format!("> "))};
     ($($arg:tt)*) =>  { $crate::_read_input(format!("{}", format_args!($($arg)*)))};
@@ -43,6 +51,7 @@ macro_rules! read_input {
 
 #[cfg(feature = "input")]
 #[macro_export]
+/// Works the same way as read_input!()
 /// Returns true if the user types 'y', 'Y', or "yes" to the prompt
 /// Returns false if the user types 'n', 'N', or "no" to the prompt
 /// repeats prompt until user enters a valid input
